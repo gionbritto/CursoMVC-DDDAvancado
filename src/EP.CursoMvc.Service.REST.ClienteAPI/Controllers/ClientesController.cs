b400add4d5ddcf7d182104cd570cfda5755e1f46
gionbritto <gio.nbritto@gmail.com>
2019-11-06 08:47:14 -0300

    Finaização de partes básicas do projeto de teste com mock e Criação do Projeto Services APIClientes

---
+++ src/EP.CursoMvc.Service.REST.ClienteAPI/Controllers/ClientesController.cs
@@ -0,0 +1,44 @@
+﻿using EP.CursoMvc.Application.Interfaces;
+using EP.CursoMvc.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace EP.CursoMvc.Service.REST.ClienteAPI.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private readonly IClienteAppService _clienteAppService;
+        public ClientesController(IClienteAppService clienteAppService)
+        {
+            _clienteAppService = clienteAppService;
+        }
+        // GET: api/Clientes
+        [HttpGet]
+        public IEnumerable<ClienteViewModel> ListarTodos()
+        {
+            return _clienteAppService.ObterTodos();
+        }
+
+        // GET: api/Clientes/5
+        public ClienteViewModel Get(Guid id)
+        {
+            return _clienteAppService.ObterPorId(id);
+        }
+
+        // POST: api/Clientes
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Clientes/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Clientes/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ tests/EP.CursoMvc.Domain.Tests/Validation/ClienteAptoParaCadastroTeste.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using EP.CursoMvc.Domain.Entities;
+using EP.CursoMvc.Domain.Interfaces.Repository;
+using EP.CursoMvc.Domain.Validations.Clientes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+
+namespace EP.CursoMvc.Domain.Tests.Validation
+{
+    [TestClass]
+    public class ClienteAptoParaCadastroTeste
+    {
+        public Cliente Cliente { get; set; }
+        [TestMethod]
+        public void ClienteApto_Validation_True()
+        {
+            Cliente = new Cliente()
+            {
+                CPF = "30390600822",
+                Email = "cad@cad.com.br"
+            };
+            Cliente.Enderecos.Add(new Endereco());
+
+            //vou gerar um mock de um repositorio (quem conecta no banco seria o IClienteRepository) 
+            //lembrando que não uso implementações e sim abstrações
+            //**Um Stub é uma simulação do tipo
+            var stupRepo = MockRepository.GenerateStub<IClienteRepository>();
+            //como se estivesse pedindo para achar um cara com esse email e falo o resultado
+            //vicio ele dizendo q vai retornar nulo
+            stupRepo.Stub(s => s.ObterPorEmail(Cliente.Email)).Return(null);
+            stupRepo.Stub(s => s.ObterPorCpf(Cliente.CPF)).Return(null);
+
+            var cliValidation = new ClienteAptoParaCadastroValidation(stupRepo);
+            Assert.IsTrue(cliValidation.Validate(Cliente).IsValid);
+        }
+
+        [TestMethod]
+        public void ClienteApto_Validation_False()
+        {
+            Cliente = new Cliente()
+            {
+                CPF = "30390600822",
+                Email = "cad@cad.com.br"
+            };
+            //Cliente.Enderecos.Add(new Endereco());
+
+            var stupRepo = MockRepository.GenerateStub<IClienteRepository>();
+            
+            stupRepo.Stub(s => s.ObterPorEmail(Cliente.Email)).Return(Cliente);
+            stupRepo.Stub(s => s.ObterPorCpf(Cliente.CPF)).Return(Cliente);
+
+            var cliValidation = new ClienteAptoParaCadastroValidation(stupRepo);
+            var result = cliValidation.Validate(Cliente);
+
+            Assert.IsFalse(cliValidation.Validate(Cliente).IsValid);
+            Assert.IsTrue(result.Erros.Any(e => e.Message == "CPF Já cadastrado"));
+            Assert.IsTrue(result.Erros.Any(e => e.Message == "E -mail já cadastrado!"));
+            Assert.IsTrue(result.Erros.Any(e => e.Message == "Cliente deve possuir pelo menos um endereço!"));
+            
+
+        }
+    }
+}
